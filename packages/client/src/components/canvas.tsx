24a4537aa6b16efe626a3960ee1e5079e445605e
87Prashant <87KumarPrashant@gmail.com>
2022-08-30 23:03:18 +0530

    add emotion/styled and canvas

---
+++ packages/client/src/components/canvas.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Canvas = () => {
+  const StyledWrapper = styled("canvas")({
+    border: "105px solid black",
+  });
+  return <StyledWrapper />;
+};
+
+export default Canvas;
