f2c1bfb76e20fc3025e29880b31932ff08ccecb5
87Prashant <87KumarPrashant@gmail.com>
2022-09-16 23:10:53 +0530

    set up d3 [2]

--- packages/client/src/components/canvas.tsx
+++ packages/client/src/components/canvas.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import styled from "@emotion/styled";
+import useD3 from '../hooks/useD3'
 
 const StyledWrapper = styled("canvas")({
   border: "10px solid black",
@@ -8,25 +9,33 @@ const StyledWrapper = styled("canvas")({
 });
 
 const Canvas = () => {
-  const canvasRef = useRef(null)
+  // const canvasRef = useRef(null)
   
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const draw = (ctx: CanvasRenderingContext2D | null) => {
-    ctx?.clearRect(0, 0, ctx?.canvas.width, ctx?.canvas.height)
-    ctx?.beginPath();
-    ctx?.rect(20, 20, 100, 10);
-    ctx?.stroke();
-  }
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // const draw = (ctx: CanvasRenderingContext2D | null) => {
+  //   ctx?.clearRect(0, 0, ctx?.canvas.width, ctx?.canvas.height)
+  //   ctx?.beginPath();
+  //   ctx?.rect(20, 20, 100, 10);
+  //   ctx?.stroke();
+  // }
 
 
-  useEffect(() => {
-    const canvas = canvasRef.current as unknown as HTMLCanvasElement
-    const context = canvas?.getContext('2d')
-    draw(context)
-    return () => {
+  // useEffect(() => {
+  //   const canvas = canvasRef.current as unknown as HTMLCanvasElement
+  //   const context = canvas?.getContext('2d')
+  //   draw(context)
+  //   return () => {
+  //   }
+  // }, [])
+
+  const canvasRef = useD3(
+    (ctx) => {
+      ctx?.clearRect(0, 0, ctx?.canvas.width, ctx?.canvas.height)
+      ctx?.beginPath();
+      ctx?.rect(20, 20, 100, 10);
+      ctx?.stroke();
     }
-  }, [])
-
+  )
   return (
     <div>
       <StyledWrapper ref={canvasRef} />
--- packages/client/src/hooks/useD3.ts
+++ packages/client/src/hooks/useD3.ts
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import * as d3 from 'd3'
 
-const useD3 = () => {
-    
+const useD3 = (callbackFunction: { (ctx: any): void }) => {
+    const ref = useRef(null)
+    const canvasRef = ref.current
+    const context = canvasRef?.getContext('2d')
+    callbackFunction(context)
+
+    return canvasRef
 }
 
 export default useD3
