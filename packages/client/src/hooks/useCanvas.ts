51bf7579acfdd245d601b0264c7f542f3589120f
87Prashant <87KumarPrashant@gmail.com>
2022-09-17 22:15:20 +0530

    remove useD3 and useCanvas

--- packages/client/src/hooks/useCanvas.ts
+++
@@ -1,9 +0,0 @@
-import React, { useRef } from 'react'
-
-// not currently used anywhere
-const useCanvas = (draw: any) => {
-    const canvasRef = useRef(null)
-    return canvasRef
-}
-
-export default useCanvas
