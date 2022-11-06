53528f514706b4af5405287f483c67e5f9de8a20
87Prashant <87KumarPrashant@gmail.com>
2022-11-06 15:48:38 +0530

    add warning on demo button

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "@emotion/styled";
 import { FormDataType } from "./Form";
 
@@ -56,6 +56,17 @@ const DemoButton = styled("button")<{ isDemoActive: boolean }>(
   })
 );
 
+const WarningTooltip = styled("div")({
+  position: "absolute",
+  width: "10",
+  height: "30",
+  top: 55,
+  right: 70,
+  color: "red",
+  fontSize: 13,
+  display: "none",
+});
+
 interface Props {
   setShowForm: React.Dispatch<React.SetStateAction<boolean>>;
   isDemoActive: boolean;
@@ -74,15 +85,21 @@ const Header = (props: Props) => {
     demoData,
     setIsChartAdded,
   } = props;
+  const ref = useRef<HTMLDivElement | null>(null);
   const showForm = () => {
     setShowForm(true);
   };
 
   function handleClick() {
     setIsDemoActive((isDemoActive: boolean) => (isDemoActive ? false : true));
-    //"!isDemoActive" because I am updating isDemoActive at the same above
+    //"!isDemoActive" because I am updating isDemoActive at the same time above
     setSavedData((prev: FormDataType[]) => (!isDemoActive ? demoData : []));
     setIsChartAdded((prev: boolean) => false);
+  }
+
+  function showWarning() {
+    if (isDemoActive) return;
+    ref.current!.style.display = "block";
   }
 
   return (
@@ -94,9 +111,17 @@ const Header = (props: Props) => {
         <img src="github_logo.png" alt="" width="25" height="25" />
         <span>Help in Development</span>
       </HelpButton>
-      <DemoButton isDemoActive={isDemoActive} onClick={handleClick}>
+      <DemoButton
+        isDemoActive={isDemoActive}
+        onClick={handleClick}
+        onMouseOver={showWarning}
+        onMouseOut={() => {
+          ref.current!.style.display = "none";
+        }}
+      >
         Demo
       </DemoButton>
+      <WarningTooltip ref={ref}>Your progress will get lost</WarningTooltip>
       <AddButton onClick={() => showForm()}>Add</AddButton>
     </StyledHeader>
   );
