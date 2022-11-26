496b1d122bc85ecde771fa4061247dc43e997066
87Prashant <87KumarPrashant@gmail.com>
2022-11-26 20:47:36 +0530

    connect with mongodb

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
@@ -6,5 +7,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/api', function (req, res) {
   res.json({"data": ["it is working...", "yes"]});
 });
+
+async function connection() {
+  mongoose.connect('mongodb://localhost:27017/local')
+}
 
 app.listen(process.env.PORT || 8000);
