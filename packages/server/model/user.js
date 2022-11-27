a4f0c5e4e206f1da5db6cd2ef03bdfdc2f3fb291
87Prashant <87KumarPrashant@gmail.com>
2022-11-27 16:17:34 +0530

    add user schema in mongodb

---
+++ packages/server/model/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    collection: "users",
+  }
+
+  );
+  
+  const model = mongoose.model("UserSchema", UserSchema)
+
+  module.exports = model
--- packages/server/server.js
+++ packages/server/server.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const User = require("./model/user");
 
-app.post('/register', function (req, res) {
-  res.json({"data": ["it is working...", "yes"]});
-});
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(bodyParser.json());
 
 async function connection() {
-  mongoose.connect('mongodb://localhost:27017/userCredentials')
+  mongoose.connect("mongodb://localhost:27017/userCredentials");
 }
 
-app.listen(process.env.PORT || 8000);
+app.post("/register", async function (req, res) {
+  
+  res.json("it is working...");
+});
+
+app.listen(8000);
