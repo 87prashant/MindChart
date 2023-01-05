b3b36c98b646947e9d95af943356b46499698d93
87Prashant <87KumarPrashant@gmail.com>
2023-01-05 23:57:56 +0530

    add generate Unique Url

--- packages/server/mail/RegistrationMail.jsx
+++ packages/server/mail/RegistrationMail.jsx
@@ -1,22 +1,20 @@
 const React = require("react");
 const ReactDOMServer = require("react-dom/server");
 
-require("dotenv").config({ path: "../../../.env" });
-
 const RegistrationMail = (props) => {
-  const { username, email, password } = props;
+  const { username, uniqueUrl } = props;
 
   return (
     <div>
       <p>Hello {username}, this is registration mail</p>
-      <button href={`/verify-email/${generateUniqueUrl}`}>Verify</button>
+      <button href={uniqueUrl}>Verify</button>
     </div>
   );
 };
 
-const bodyContent = ({ username, email, password }) => {
+const bodyContent = ({ username, uniqueUrl }) => {
   return ReactDOMServer.renderToString(
-    <RegistrationMail username={username} email={email} password={password} />
+    <RegistrationMail username={username} uniqueUrl={uniqueUrl} />
   );
 };
 
