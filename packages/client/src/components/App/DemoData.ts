642d4b96040d9aa44aee27cc7dcf1c5ff4727774
87Prashant <87KumarPrashant@gmail.com>
2023-01-15 22:36:53 +0530

    cleanup

---
+++ packages/client/src/components/App/DemoData.ts
@@ -0,0 +1,206 @@
+import { FormDataType } from "../Form";
+
+export const demoData: FormDataType[] = [
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        neutral: 10,
+      },
+      priority: 10,
+      description: "Random text 1 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        unknown: false,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+      },
+      emotions: {
+        neutral: 10,
+      },
+      priority: 16,
+      description: "Random text 2 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: true,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        neutral: 10,
+      },
+      priority: 25,
+      description: "Random text 3 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        neutral: 10,
+      },
+      priority: 15,
+      description: "Random text 4 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        neutral: 10,
+      },
+      priority: 19,
+      description: "Random text 5 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        joy: 10,
+      },
+      priority: 50,
+      description: "Random text 6 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        anticipation: 10,
+      },
+      priority: 27,
+      description: "Random text 7 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        fear: 10,
+      },
+      priority: 37,
+      description: "Random text 8 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        fear: 10,
+      },
+      priority: 70,
+      description: "Random text 9 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        sadness: 10,
+      },
+      priority: 12,
+      description: "Random text 10 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        trust: 10,
+      },
+      priority: 34,
+      description: "Random text 11 for demo",
+    },
+    {
+      categories: {
+        creative: false,
+        concrete: false,
+        abstract: false,
+        analytical: true,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        trust: 10,
+        anticipation: 23,
+        joy: 15,
+        fear: 26,
+        neutral: 32,
+        sadness: 12,
+        anger: 50,
+      },
+      priority: 12,
+      description: "Random text 12 for demo",
+    },
+    {
+      categories: {
+        creative: true,
+        concrete: false,
+        abstract: false,
+        analytical: false,
+        critical: false,
+        unknown: false,
+      },
+      emotions: {
+        surprise: 10,
+      },
+      priority: 10,
+      description:
+        "Random text 9 for demo Random text 9 for demo Random text 9 for demo Random text 9 for demo Random text 9 for demo Random text 9 for demo Random text 9 for demo Random text 9 for demo Random text 9 for demo ",
+    },
+  ];
