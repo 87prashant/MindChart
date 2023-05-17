cd9b44698ed91e8373e16cb3383dcfac45bf3697
87Prashant <87KumarPrashant@gmail.com>
2023-05-18 00:05:41 +0530

    refactor email verification api

---
+++ packages/server/routes/EmailVerification.ts
@@ -0,0 +1,87 @@
+import {
+  AccountStatus,
+  ErrorMessage,
+  LogLevel,
+  Message,
+  ResponseStatus,
+} from "../constants";
+import logger from "../logger";
+import { UserType, createUserData, session } from "../server";
+import User from "../model/user";
+
+const EmailVerification = async (req, res) => {
+  const { email, verificationToken } = req.body;
+  logger(
+    `Request received for email verification, email: ${email}`,
+    LogLevel.INFO
+  );
+  let user: UserType;
+  try {
+    user = await User.findOne({ email }).lean();
+  } catch (error) {
+    logger(
+      `${ErrorMessage.UNABLE_TO_FIND_USER}, email: ${email}`,
+      LogLevel.ERROR,
+      error
+    );
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.SERVER_ERROR,
+    });
+  }
+
+  // Authorization
+  if (!user) {
+    logger(`${ErrorMessage.NOT_REGISTERED}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.NOT_REGISTERED,
+    });
+  }
+  if (user.status !== AccountStatus.UNVERIFIED) {
+    logger(`${ErrorMessage.NOT_ALLOWED}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.NOT_ALLOWED,
+    });
+  }
+  if (user.verificationToken !== verificationToken) {
+    logger(`${ErrorMessage.INVALID_TOKEN}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.INVALID_TOKEN,
+    });
+  }
+
+  // Update user as verified
+  // Start the transaction
+  session.startTransaction();
+  try {
+    await User.updateMany(
+      { email },
+      { $unset: { verificationToken: "", status: "" } }, // no status means verified
+      { session }
+    );
+    await createUserData(email, session);
+    session.commitTransaction();
+    logger(`${Message.VERIFY_SUCCESS}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.OK,
+      username: user.username,
+      imageUrl: user.imageUrl,
+    });
+  } catch (error) {
+    session.abortTransaction();
+    logger(
+      `${ErrorMessage.UNABLE_TO_UPDATE_USER}, email: ${email}`,
+      LogLevel.ERROR,
+      error
+    );
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.SERVER_ERROR,
+    });
+  }
+};
+
+export default EmailVerification;
--- packages/server/server.ts
+++ packages/server/server.ts
@@ -32,8 +32,9 @@ import {
 } from "./constants";
 import { ClientSession } from "mongodb";
 import { Document } from "mongoose";
-import GoogleAuth from "./routes/GoogleAuth"
-import Register from "./routes/Register"
+import GoogleAuth from "./routes/GoogleAuth";
+import Register from "./routes/Register";
+import EmailVerification from "./routes/EmailVerification";
 
 const app = express();
 
@@ -161,80 +162,7 @@ app.post("/register", Register);
 /**
  * @api Email verification
  */
-app.post("/verify-email", async function (req, res) {
-  const { email, verificationToken } = req.body;
-  logger(
-    `Request received for email verification, email: ${email}`,
-    LogLevel.INFO
-  );
-  let user: UserType;
-  try {
-    user = await User.findOne({ email }).lean();
-  } catch (error) {
-    logger(
-      `${ErrorMessage.UNABLE_TO_FIND_USER}, email: ${email}`,
-      LogLevel.ERROR,
-      error
-    );
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.SERVER_ERROR,
-    });
-  }
-
-  // Authorization
-  if (!user) {
-    logger(`${ErrorMessage.NOT_REGISTERED}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.NOT_REGISTERED,
-    });
-  }
-  if (user.status !== AccountStatus.UNVERIFIED) {
-    logger(`${ErrorMessage.NOT_ALLOWED}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.NOT_ALLOWED,
-    });
-  }
-  if (user.verificationToken !== verificationToken) {
-    logger(`${ErrorMessage.INVALID_TOKEN}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.INVALID_TOKEN,
-    });
-  }
-
-  // Update user as verified
-  // Start the transaction
-  session.startTransaction();
-  try {
-    await User.updateMany(
-      { email },
-      { $unset: { verificationToken: "", status: "" } }, // no status means verified
-      { session }
-    );
-    await createUserData(email, session);
-    session.commitTransaction();
-    logger(`${Message.VERIFY_SUCCESS}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.OK,
-      username: user.username,
-      imageUrl: user.imageUrl,
-    });
-  } catch (error) {
-    session.abortTransaction();
-    logger(
-      `${ErrorMessage.UNABLE_TO_UPDATE_USER}, email: ${email}`,
-      LogLevel.ERROR,
-      error
-    );
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.SERVER_ERROR,
-    });
-  }
-});
+app.post("/verify-email", EmailVerification);
 
 /**
  * @api Forget password
