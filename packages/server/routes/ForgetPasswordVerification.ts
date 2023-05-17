cf0447a9db5f6df5289e5a133a8e8f6b25b6b989
87Prashant <87KumarPrashant@gmail.com>
2023-05-18 00:13:12 +0530

    refactor the Forget Password Verification Api

---
+++ packages/server/routes/ForgetPasswordVerification.ts
@@ -0,0 +1,110 @@
+import {
+  AccountStatus,
+  ErrorMessage,
+  LogLevel,
+  Message,
+  ResponseStatus,
+} from "../constants";
+import logger from "../logger";
+import { UserType, getUserData, session } from "../server";
+import User from "../model/user";
+import bcrypt from "bcryptjs";
+
+const ForgetPasswordVerification = async (req, res) => {
+  const { email, verificationToken, password: plainPassword } = req.body;
+  logger(
+    `Request received for password reset verification, email:  ${email}`,
+    LogLevel.INFO
+  );
+
+  // Validation
+  if (plainPassword.length < 8) {
+    logger(`${ErrorMessage.SHORT_PASSWORD}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.SHORT_PASSWORD,
+    });
+  }
+
+  let user: UserType;
+  try {
+    user = await User.findOne({ email }).lean();
+  } catch (error) {
+    logger(
+      `${ErrorMessage.UNABLE_TO_FIND_USER}, email: ${email}`,
+      LogLevel.ERROR,
+      error
+    );
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.SERVER_ERROR,
+    });
+  }
+
+  // Authorization
+  if (!user) {
+    logger(`${ErrorMessage.NOT_REGISTERED}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.NOT_REGISTERED,
+    });
+  }
+  if (!user.status) {
+    logger(`${ErrorMessage.NOT_ALLOWED}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.NOT_ALLOWED,
+    });
+  } else if (user.status == AccountStatus.UNVERIFIED) {
+    logger(`${ErrorMessage.NOT_VERIFIED}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.NOT_VERIFIED,
+    });
+  } else if (user.verificationToken !== verificationToken) {
+    logger(`${ErrorMessage.INVALID_TOKEN}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.INVALID_TOKEN,
+    });
+  }
+
+  // Change password
+  // Start the transaction
+  session.startTransaction();
+  const password = await bcrypt.hash(plainPassword, 10);
+  try {
+    await User.updateMany(
+      { email },
+      { $unset: { status: "", verificationToken: "" } },
+      { session }
+    );
+    await User.updateMany(
+      { email },
+      { $set: { password: password } },
+      { session }
+    ); // why not working in one query?
+    const userData = await getUserData(email);
+    session.commitTransaction();
+    logger(`${Message.VERIFY_SUCCESS}, email: ${email}`, LogLevel.INFO);
+    return res.json({
+      status: ResponseStatus.OK,
+      username: user.username,
+      imageUrl: user.imageUrl,
+      userData,
+    });
+  } catch (error) {
+    session.abortTransaction();
+    logger(
+      `${ErrorMessage.UNABLE_TO_UPDATE_USER} to reset password, email: ${email}`,
+      LogLevel.ERROR,
+      error
+    );
+    return res.json({
+      status: ResponseStatus.ERROR,
+      error: ErrorMessage.SERVER_ERROR,
+    });
+  }
+};
+
+export default ForgetPasswordVerification;
--- packages/server/server.ts
+++ packages/server/server.ts
@@ -32,6 +32,7 @@ import GoogleAuth from "./routes/GoogleAuth";
 import Register from "./routes/Register";
 import EmailVerification from "./routes/EmailVerification";
 import ForgetPassword from "./routes/ForgetPassword";
+import ForgetPasswordVerification from "./routes/ForgetPasswordVerification";
 
 const app = express();
 
@@ -169,102 +170,7 @@ app.post("/forget-password", ForgetPassword);
 /**
  * @api Forget password verification
  */
-app.post("/forget-password-verify", async function (req, res) {
-  const { email, verificationToken, password: plainPassword } = req.body;
-  logger(
-    `Request received for password reset verification, email:  ${email}`,
-    LogLevel.INFO
-  );
-
-  // Validation
-  if (plainPassword.length < 8) {
-    logger(`${ErrorMessage.SHORT_PASSWORD}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.SHORT_PASSWORD,
-    });
-  }
-
-  let user: UserType;
-  try {
-    user = await User.findOne({ email }).lean();
-  } catch (error) {
-    logger(
-      `${ErrorMessage.UNABLE_TO_FIND_USER}, email: ${email}`,
-      LogLevel.ERROR,
-      error
-    );
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.SERVER_ERROR,
-    });
-  }
-
-  // Authorization
-  if (!user) {
-    logger(`${ErrorMessage.NOT_REGISTERED}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.NOT_REGISTERED,
-    });
-  }
-  if (!user.status) {
-    logger(`${ErrorMessage.NOT_ALLOWED}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.NOT_ALLOWED,
-    });
-  } else if (user.status == AccountStatus.UNVERIFIED) {
-    logger(`${ErrorMessage.NOT_VERIFIED}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.NOT_VERIFIED,
-    });
-  } else if (user.verificationToken !== verificationToken) {
-    logger(`${ErrorMessage.INVALID_TOKEN}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.INVALID_TOKEN,
-    });
-  }
-
-  // Change password
-  // Start the transaction
-  session.startTransaction();
-  const password = await bcrypt.hash(plainPassword, 10);
-  try {
-    await User.updateMany(
-      { email },
-      { $unset: { status: "", verificationToken: "" } },
-      { session }
-    );
-    await User.updateMany(
-      { email },
-      { $set: { password: password } },
-      { session }
-    ); // why not working in one query?
-    const userData = await getUserData(email);
-    session.commitTransaction();
-    logger(`${Message.VERIFY_SUCCESS}, email: ${email}`, LogLevel.INFO);
-    return res.json({
-      status: ResponseStatus.OK,
-      username: user.username,
-      imageUrl: user.imageUrl,
-      userData,
-    });
-  } catch (error) {
-    session.abortTransaction();
-    logger(
-      `${ErrorMessage.UNABLE_TO_UPDATE_USER} to reset password, email: ${email}`,
-      LogLevel.ERROR,
-      error
-    );
-    return res.json({
-      status: ResponseStatus.ERROR,
-      error: ErrorMessage.SERVER_ERROR,
-    });
-  }
-});
+app.post("/forget-password-verify", ForgetPasswordVerification);
 
 /**
  * @api Login user
